22eb00509e2b627667f015a5758b5f396d8effe4
Mehedi Rahman Mahi <mehedirm6244@protonmail.com>
2025-03-07 16:48:47 +0600

    Crucial changes to the algorithm

--- ALGORITHM.md
+++ ALGORITHM.md
@@ -5,7 +5,7 @@ Note: This algorithm probably exists somewhere already, under a different name.
 
 This algorithm reduces file size by encoding unique characters with a variable-length bit representation, instead of the standard 8-bit ASCII encoding.
 
-- Identify the number of unique characters in the input file (*n*)
+- Identify the number of unique characters (*n*) and total characters in the input file 
 - Determine the minimum number of bits required to represent each unique character. It can be shown that the number is *ceil(log2(n))*
 - Replace each character in the input text with its corresponding compressed representation.
 - **The effectiveness of this compression increases when the number of unique characters is small and the file size is large.**
@@ -14,10 +14,11 @@ This algorithm reduces file size by encoding unique characters with a variable-l
 
 The compressed file is stored in binary format, structured as follows:
 
-- Header/ Metadata:
-  - The **first 8 bits** store the number of unique characters (*n*).
-  - The **next n bytes** (*n × 8* bits) store the ASCII values of the unique characters, sorted in ascending order.
-- Compressed Data
+- **Header/ Metadata**:
+  - The **first byte** stores the number of unique characters (*n*).
+  - The **next 4 bytes** store the number of total characters (32 bit integer).
+  - The **next n bytes** (each byte representing an 8-bit ASCII value) store the unique characters, sorted in ascending order.
+- **Compressed Data**
   - Each character from the original text is encoded using *ceil(log2(n))* bits instead of the standard 8 bits.
 
 ### Sample Input
@@ -27,45 +28,34 @@ cse paina public versity te aaaaaaaaaaaaaaaaaaaaa
 - Unique characters: 15
 - Size: 49 bytes
 
-### Compressed Binary Represantation (newlines added for clarity)
+### Compressed Representation (newlines added for clarity)
 ```
-00001111
-00100000
-01100001
-01100010
-01100011
-01100101
-01101001
-01101100
-01101110
-01110000
-01110010
-01110011
-01110100
-01110101
-01110110
-01111001
-0011101001000000100000010101011100010000100011000010011001010011000011010100100110100101101111100000101101000000000100010001000100010001000100010001000100010001000100010001000100010001000100010001
+15
+0 0 0 49
+32 97 98 99 101 105 108 110 112 114 115 116 117 118 121
+58 64 129 87 16 140 38 83 13 73 165 190 11 64 17 17 17 17 17 17 17 17 17 17 16 
 ```
-- The first 8 bits (`00001111`) = *15* is the number of the unique characters.
-- Next *15* bytes are the  The ASCII values of the unique characters, stored in increasing order: `[ ' ',  'a', 'b', 'c', 'e', 'i', 'l', 'n', 'p', 'r', 's', 't', 'u', 'v', 'y' ]`
+- The **first byte**, *15*, is the number of the unique characters.
+- The **next 4 bytes** represent the number of total characters in binary format (`00000000 00000000 00000000 00110001` = *49*)
+- The **next *15* bytes** are the ASCII values of the unique characters, stored in increasing order: `[ ' ',  'a', 'b', 'c', 'e', 'i', 'l', 'n', 'p', 'r', 's', 't', 'u', 'v', 'y' ]`
 - **Character Encoding:**
   - *ceil(log2(15)) = 4* bits are enough to represent 15 unique characters.
   - The next *(4 x 49) = 196* bits store the compressed data
 
 ### Compression Efficiency
 - Original size: *49* bytes
-- Compressed size: *41* bytes
-- Compression is more effective when the input has fewer unique characters and more repetitions.
+- Compressed size: *45* bytes
+- **Compression is more effective when the input has fewer unique characters and more repetitions.**
 
 
 ## Decompression
 
 The decompression process reverses the compression steps:
 
-- Read the first *8* bits to determine the number of unique characters (*n*).
-- Read the next *n × 8* bits to reconstruct the unique character set.
-- Read the rest of the file in *ceil(log2(15))*-bit chunks to decode the original text.
+- Read the **first byte** to determine the number of unique characters (*n*).
+- Read the **next 4 bytes** to determine the number of total characters.
+- Read the **next *n* bytes** to reconstruct the unique character set.
+- Read the rest of the file in *ceil(log2(n))*-bit chunks to decode the original text.
 - Convert each chunk back into its corresponding ASCII character using the stored dictionary.
 
 ## Summary
--- src/KP_Compressor.cpp
+++ src/KP_Compressor.cpp
@@ -91,14 +91,15 @@ bool KP_Compressor::decompress_file(std::string& filepath) {
  */
 std::vector<uint8_t> KP_Compressor::compress(std::string& data) {
   std::set<char> unique_chars(data.begin(), data.end());
-  int unique_char_cnt = unique_chars.size();
+  size_t unique_char_cnt = unique_chars.size();
+  size_t char_cnt = data.size();
 
   // Maximum number of bits required to represent each unique character
   int bit_size = std::max((int)(ceil(log2(unique_char_cnt))), 1);
 
   // Create a mapping from each unique character to a unique index
   std::map<char, int> char_map;
-  int current_ch_idx = 0;
+  size_t current_ch_idx = 0;
   for (auto ch : unique_chars) {
     char_map[ch] = current_ch_idx++;
   }
@@ -106,35 +107,45 @@ std::vector<uint8_t> KP_Compressor::compress(std::string& data) {
   /**
    * Calculate the size of the compressed data (vector) in bytes
    * - 1 byte for the number of unique characters
+   * - 4 bytes for the number of total characters
    * - `unique_char_cnt` bytes for the unique characters themselves
-   * - `data.size() * bit_size` bits for the encoded data
+   * - `char_cnt * bit_size` bits for the encoded data
    */
-  int compressed_data_size = (1 + unique_char_cnt) * 8 + data.size() * bit_size; // By bits
+  size_t compressed_data_size = (5 + unique_char_cnt) * 8 + char_cnt * bit_size; // By bits
   compressed_data_size = ceil(compressed_data_size / 8.0); // By bytes
   std::vector<uint8_t> compressed_data(compressed_data_size, 0);
+  size_t compressed_data_idx;
+  size_t bit_pos = 0; // Current bit position within a byte
 
+  // The number of unique characters
   compressed_data[0] = static_cast<uint8_t>(unique_char_cnt);
-  current_ch_idx = 1;
+
+  // The number of total characters, split into four bytes
+  compressed_data[1] = static_cast<uint8_t>((char_cnt >> 24) & 0xFF);
+  compressed_data[2] = static_cast<uint8_t>((char_cnt >> 16) & 0xFF);
+  compressed_data[3] = static_cast<uint8_t>((char_cnt >> 8) & 0xFF);
+  compressed_data[4] = static_cast<uint8_t>(char_cnt & 0xFF);
+
+  // A byte for each unique characters
+  compressed_data_idx = 5;
   for (auto ch : unique_chars) {
-    compressed_data[current_ch_idx++] = ch;
+    compressed_data[compressed_data_idx++] = ch;
   }
 
-  int compressed_vec_idx = current_ch_idx;
-  int bit_pos = 0; // Current bit position within a byte
-
+  // Actual encoded data
   for (char ch : data) {
-    int encoded_value = char_map[ch];
+    unsigned int encoded_value = char_map[ch];
 
-    // Write the encoded value into the compressed data using `bit_size` bits
+    // Write the encoded value into `compressed_data` using `bit_size` bits
     for (int i = bit_size - 1; i >= 0; i--) {
       if ((encoded_value >> i) & 1) {
-        compressed_data[compressed_vec_idx] |= (1 << (7 - bit_pos));
+        compressed_data[compressed_data_idx] |= (1 << (7 - bit_pos));
       }
       bit_pos++;
       if (bit_pos == 8) {
-        // Move to the next byte if the byte is filled
+        // Move to next byte if the byte is filled
         bit_pos = 0;
-        compressed_vec_idx++;
+        compressed_data_idx++;
       }
     }
   }
@@ -147,20 +158,31 @@ std::vector<uint8_t> KP_Compressor::compress(std::string& data) {
  * https://github.com/mehedirm6244/kompreshar/blob/main/ALGORITHM.md#decompression
  */
 std::string KP_Compressor::decompress(const std::vector<uint8_t>& bin_data) {
-  int unique_char_cnt = bin_data[0];
+  size_t unique_char_cnt = bin_data[0];
+
+  // Total number of characters of the uncompressed file
+  uint32_t char_cnt = (static_cast<uint32_t>(bin_data[1]) << 24) |
+                      (static_cast<uint32_t>(bin_data[2]) << 16) |
+                      (static_cast<uint32_t>(bin_data[3]) << 8) |
+                      bin_data[4];
 
   // The number of bits used to represent each character during compression
-  int bit_size = std::max((int)(ceil(log2(unique_char_cnt))), 1);
+  size_t bit_size = std::max((int)(ceil(log2(unique_char_cnt))), 1);
 
   std::map<int, char> char_map;
-  for (int i = 1; i <= unique_char_cnt; i++) {
-    char_map[i - 1] = static_cast<char>(bin_data[i]);
+  for (int i = 0; i < unique_char_cnt; i++) {
+    char_map[i] = static_cast<char>(bin_data[5 + i]);
   }
 
   std::string decompressed_data;
-  int bit_pos = 0; // Current bit position within a byte
+  size_t bit_pos = 0; // Current bit position within a byte
+  size_t decompressed_char_cnt = 0;
 
-  for (int i = unique_char_cnt + 1; i < bin_data.size(); ) {
+  for (int i = 5 + unique_char_cnt; i < bin_data.size(); ) {
+    if (decompressed_char_cnt == char_cnt) {
+      break;
+    }
+
     int compressed_code = 0;
 
     // Decode the next `bit_size` bits to get the compressed code
@@ -175,6 +197,7 @@ std::string KP_Compressor::decompress(const std::vector<uint8_t>& bin_data) {
       }
       bit_pos++;
     }
+    decompressed_char_cnt++;
 
     if (char_map.find(compressed_code) != char_map.end()) {
       decompressed_data += char_map[compressed_code];
